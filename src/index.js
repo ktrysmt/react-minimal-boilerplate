b3f65b63cc8696695987108b2cd3eef888c13089
ktrysmt <kotaro.yoshimatsu@gmail.com>
2016-07-06 17:57:48 +0900

    Fix index.js by reference to ESLint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Main from './components/main.jsx';
 
 ReactDOM.render(
   <Main />,
   document.getElementById('app')
-)
+);
